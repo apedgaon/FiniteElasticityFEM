ba245ffbe0ad4518bf3a7888495801a90723e61a
Akhilesh Pedgaonkar <pedgaonkar@wisc.edu>
2022-03-05 22:51:29 -0600

    some minor comments and pragma

--- FSElasticityFEM.h
+++ FSElasticityFEM.h
@@ -1,6 +1,7 @@
+#pragma once
 #include "FEMUtility.h"
 
-
+// Finite Strain Elasticity Finite Element Method Class
 template <unsigned int dim>
 class FSElasticityFEM
 {
@@ -12,6 +13,7 @@ public:
     Material mat;
     Mesh<dim> mesh;
     std::vector<DirichletBC> dirBCs;
+
 private:
     void compute_elem_quantities(connectivity& conn, Eigen::VectorXui& gidx, Eigen::MatrixXd& xe, Eigen::VectorXd& de);
     void compute_quad_derivatives(unsigned int iq, Eigen::MatrixXd& xe, Eigen::VectorXd& de, Eigen::MatrixXd& jac,
@@ -21,6 +23,7 @@ private:
     void compute_quad_res(unsigned int iq, Eigen::MatrixXd& dNdX, Eigen::MatrixXd& dfgrd, Eigen::MatrixXd& S_pk2,
                           double det_jac, Eigen::MatrixXd& Rq);
 
+private:
     Eigen::VectorXd d;
     Eigen::VectorXd dd;
 
