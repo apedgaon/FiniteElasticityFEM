502354cd2f0a23c8db870daef9dc639223fbe74d
Akhilesh Pedgaonkar <pedgaonkar@wisc.edu>
2022-03-16 16:46:57 -0500

    add comments in problem file

--- FSProblem.cpp
+++ FSProblem.cpp
@@ -5,23 +5,28 @@
 void Problem1()
 {
     std::cout << "PROBLEM 1 ANALYSIS";
+
+    // Create FEM Object
     FSElasticityFEM<3> fem;
     fem.geom.length[0] = 10.0;  // cm
     fem.geom.length[1] = 3.0;   // cm
     fem.geom.length[2] = 3.0;   // cm
 
+    // Material  parameters assignment
     fem.mat.lambda = 6.0e6;     // N cm^-2
     fem.mat.mu = 2.0e6;         // N cm^-2
     fem.mat.init_elasticity_tensor();
 
+    // Mesh settings and generation
     fem.mesh.mesh_type = MeshType::hexahedral;
     fem.mesh.quadrature_type = QuadratureType::two_point;
     fem.mesh.shp_func_type = ShapeFcnType::lagrange_linear;
     fem.mesh.divs[0] = 20;
     fem.mesh.divs[1] = 6;
     fem.mesh.divs[2] = 6;
-
     fem.generate_mesh();
+
+    // Apply boundary conditions
     for (unsigned int idx = 0; idx < fem.mesh.Nnd; ++idx)
     {
         auto& nd_coords = fem.mesh.nodal_coords[idx];
@@ -63,37 +68,45 @@ void Problem1()
         }
     }
 
+    // Solver settings
     fem.sol_ctrls.antype = AnalysisType::_static;
     fem.sol_ctrls.nsteps = 1;
     fem.sol_ctrls.ninc_max = 10;
     fem.sol_ctrls.nr_tol = 1.0e-8;
 
+    // Initialize, Solve, Post
     fem.initialize();
     fem.solve();
     fem.post_print("ProblemFS1");
+
     std::cout << "END PROBLEM 1 ANALYSIS\n\n";
 }
 
 void Problem2()
 {
     std::cout << "PROBLEM 2 ANALYSIS";
+
+    // Create FEM Object
     FSElasticityFEM<3> fem;
     fem.geom.length[0] = 10.0;  // cm
     fem.geom.length[1] = 3.0;   // cm
     fem.geom.length[2] = 3.0;   // cm
 
+    // Material  parameters assignment
     fem.mat.lambda = 6.0e6;     // N cm^-2
     fem.mat.mu = 2.0e6;         // N cm^-2
     fem.mat.init_elasticity_tensor();
 
+    // Mesh settings and generation
     fem.mesh.mesh_type = MeshType::hexahedral;
     fem.mesh.quadrature_type = QuadratureType::two_point;
     fem.mesh.shp_func_type = ShapeFcnType::lagrange_linear;
     fem.mesh.divs[0] = 20;
     fem.mesh.divs[1] = 6;
     fem.mesh.divs[2] = 6;
-
     fem.generate_mesh();
+
+    // Apply boundary conditions
     for (unsigned int idx = 0; idx < fem.mesh.Nnd; ++idx)
     {
         auto& nd_coords = fem.mesh.nodal_coords[idx];
@@ -123,20 +136,23 @@ void Problem2()
         }
     }
 
+    // Solver settings
     fem.sol_ctrls.antype = AnalysisType::_static;
     fem.sol_ctrls.nsteps = 1;
     fem.sol_ctrls.ninc_max = 10;
     fem.sol_ctrls.nr_tol = 1.0e-8;
 
+    // Initialize, Solve, Post
     fem.initialize();
     fem.solve();
     fem.post_print("ProblemFS2");
+
     std::cout << "END PROBLEM 2 ANALYSIS\n";
 }
 
 int main(int argc, char* argv[])
 {
     Problem1();
-    //Problem2();
+    Problem2();
     return 0;
 }
